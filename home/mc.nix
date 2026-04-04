6ca7506eaf5409bab6ff8250b5633c9448d66552
zp <o.email.do.ze.pedro@gmail.com>
2026-04-04 20:55:00 +0100

    Initial clean NixOS config with wallpaper+lock

--- home/mc.nix
+++ home/mc.nix
@@ -1,19 +1,31 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   imports = [
    # ../modules/.nix
   ];
 
-  home.file.".wallpaper.jpg".source = ../wallpapers/wallpaper-mc.jpg;
   home.username = "mc";
   home.homeDirectory = "/home/mc";
 
+  home.file.".wallpaper.jpg".source = ../wallpapers/wallpaper-mc.jpg;
+  home.activation.setWallpaper = lib.hm.dag.entryAfter ["writeBoundary"] ''
+  ${pkgs.qt6.qttools}/bin/qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript '
+    var Desktops = desktops();
+    for (i=0;i<Desktops.length;i++) {
+      d = Desktops[i];
+      d.wallpaperPlugin = "org.kde.image";
+      d.currentConfigGroup = Array("Wallpaper", "org.kde.image", "General");
+      d.writeConfig("Image", "file://${config.home.homeDirectory}/.wallpaper.jpg");
+    }
+  '
+'';
   home.stateVersion = "25.11"; # match your NixOS version
 
   programs.home-manager.enable = true;
 
   home.packages = with pkgs; [
     simple-scan
+    qt6.qttools
   ];
 }
