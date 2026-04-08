a61ca54c07a8dd5b126b8399793ef93bc9671e8f
zp <o.email.do.ze.pedro@gmail.com>
2026-04-08 23:39:14 +0100

    Clean NixOS config baseline v2

--- hosts/krieger/configuration.nix
+++ hosts/krieger/configuration.nix
@@ -62,7 +62,6 @@
   # Audio
   services.pulseaudio.enable = false;
   services.pipewire.enable = true;
-  services.pipewire.enable = true;
   services.pipewire.alsa.enable = true;
   services.pipewire.alsa.support32Bit = true;
   services.pipewire.pulse.enable = true;
