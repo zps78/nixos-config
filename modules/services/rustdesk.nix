16a1c52e3a566447500a019d4a94c1374711749d
zp <mostly@krugerrand>
2026-05-20 16:08:57 +0100

    Rustdesk + service

--- modules/services/rustdesk.nix
+++ modules/services/rustdesk.nix
@@ -1,85 +1,38 @@
 # ../../modules/services/rustdesk.nix
-#
-# RustDesk remote desktop module for NixOS
-#
-# Provides:
-# - RustDesk client
-# - RustDesk service
-# - Firewall configuration
-#
-# Supports:
-# - unattended access
-# - Wayland
-# - X11
-# - LAN + internet remote desktop
-#
-# Usage:
-#
-# imports = [
-#   ../../modules/services/rustdesk.nix
-# ];
-#
-# Optional:
-# - self-hosted RustDesk server
-# - Wayland desktop sharing
-#
 
 { pkgs, ... }:
 
 {
-  # -------------------------------------------------
-  # RustDesk package
-  # -------------------------------------------------
   environment.systemPackages = with pkgs; [
-    rustdesk-flutter # Virtual / remote desktop infrastructure for everyone! Open source TeamViewer / Citrix alternative
+    rustdesk
   ];
 
-  # -------------------------------------------------
-  # Enable RustDesk service
-  # -------------------------------------------------
-  services.rustdesk = {
-    enable = true;
+  networking.firewall = {
+    allowedTCPPorts = [
+      21115
+      21116
+      21117
+      21118
+      21119
+    ];
 
-    # Open required firewall ports automatically
-    openFirewall = true;
+    allowedUDPPorts = [
+      21116
+    ];
   };
 
   # -------------------------------------------------
-  # Notes
+  # RustDesk user service
   # -------------------------------------------------
-  #
-  # First launch:
-  #
-  #   rustdesk
-  #
-  # You can then:
-  # - set unattended password
-  # - connect via RustDesk ID
-  # - configure relay/server
-  #
-  # -------------------------------------------------
-  # Wayland notes
-  # -------------------------------------------------
-  #
-  # For Hyprland / Wayland:
-  #
-  # Screen sharing works best with:
-  #
-  #   xdg-desktop-portal-hyprland
-  #   pipewire
-  #
-  # If using your ml4w setup,
-  # you likely already have these.
-  #
-  # -------------------------------------------------
-  # Optional self-hosted server
-  # -------------------------------------------------
-  #
-  # RustDesk supports:
-  #
-  # - hbbs (ID server)
-  # - hbbr (relay server)
-  #
-  # Can be added later on a VPS or NAS.
-  #
+  systemd.user.services.rustdesk = {
+    description = "RustDesk Remote Desktop";
+
+    wantedBy = [ "graphical-session.target" ];
+
+    serviceConfig = {
+      ExecStart = "${pkgs.rustdesk}/bin/rustdesk";
+      Restart = "on-failure";
+      RestartSec = 5;
+    };
+  };
 }
