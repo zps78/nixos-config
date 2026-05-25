a90eedd5b6edfc80b8e3098424ccfd2289065da4
zp <mostly@krugerrand>
2026-05-25 22:09:41 +0100

    force wayland on nvidia sunshine

--- modules/services/sunshine.nix
+++ modules/services/sunshine.nix
@@ -99,7 +99,7 @@ in
     (lib.mkIf (cfg.gpuVendor == "nvidia") {
       services.sunshine.settings = {
         # NVIDIA-specific Sunshine settings
-        capture = "kms";
+        capture = "wayland";
       };
     })
   ]);
