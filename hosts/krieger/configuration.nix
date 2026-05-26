e8176e581dddbe5ed4fe527be811adcd083b0922
zp <mostly@krugerrand>
2026-05-26 13:22:50 +0100

    MTU settings for 2.5gbps nic

--- hosts/krieger/configuration.nix
+++ hosts/krieger/configuration.nix
@@ -50,7 +50,10 @@
 
   # Networking
   networking.hostName = "krieger";
-  networking.interfaces.enp4s0.wakeOnLan.enable = true; # -> only for ethernet wired hosts
+  networking.interfaces.enp4s0 = {
+    mtu = 1492;                # default 1500. try 1492 or 1450 if still unstable
+    wakeOnLan.enable = true;   # -> only for ethernet wired hosts
+  };
 
   nixpkgs.overlays = [
     (final: prev: {
