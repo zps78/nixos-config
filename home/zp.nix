d961ba47dd3f1b85bc4c4fc08a7342e6b04f3c48
zp <mostly@kuro>
2026-07-30 18:53:09 +0100

    proton apps

--- home/zp.nix
+++ home/zp.nix
@@ -39,7 +39,7 @@
     # office and productivity
     office.enable                                = true;             # Office suite that combines text, spreadsheet and presentation editors
     protonmail-desktop.enable                    = false;            # Desktop application for Mail and Calendar, made with Electron
-    proton-pass.enable                           = false;            # Desktop application for Proton Pass
+    proton-pass.enable                           = true;             # Desktop application for Proton Pass
     thunderbird.enable                           = false;            # Full-featured e-mail client
 
     # Emulation / virtualization
