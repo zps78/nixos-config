f581ae932af90d9287d90e7425ea6063f60657cd
zp <mostly@kuro>
2026-08-07 12:26:58 +0100

    vscodium extension

--- modules/apps/vscodium.nix
+++ modules/apps/vscodium.nix
@@ -11,19 +11,16 @@
         kdl = pkgs.vscode-utils.buildVscodeExtension {
           pname = "kdl";
           version = "2.1.3";
-
           src = pkgs.fetchurl {
             url = "https://open-vsx.org/api/v1hz/kdl/2.1.3/file/v1hz.kdl-2.1.3.vsix";
             hash = "sha256-i5J4hXU3cOnFiPS+LZ0fIwwQo7hV0dueF0cPXUv25z0=";
           };
-
           vscodeExtPublisher = "v1hz";
           vscodeExtName = "kdl";
           vscodeExtUniqueId = "v1hz.kdl";
         };
       in {
         enable = true;
-
         profiles.default.extensions =
           (with pkgs.vscode-extensions; [
             enkia.tokyo-night
