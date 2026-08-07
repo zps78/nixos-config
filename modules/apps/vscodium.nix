369fbe7a1f0fd041dbf915c357e1844528e25986
zp <mostly@kuro>
2026-08-07 12:12:57 +0100

    vscodium extension

--- modules/apps/vscodium.nix
+++ modules/apps/vscodium.nix
@@ -6,28 +6,36 @@
     lib.mkEnableOption "VSCodium";
 
   config = lib.mkIf config.myApps.vscodium.enable {
-    programs.vscodium = {
-      enable = true;
 
-    profiles.default.extensions =
-      (with pkgs.vscode-extensions; [
-        enkia.tokyo-night
-        esbenp.prettier-vscode
-        jeff-hykin.better-nix-syntax
-        ]
-      )
-      ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-        {
-        name = "kdl";
-        publisher = "v1hz";
+    let
+      kdl = pkgs.vscode-utils.buildVscodeExtension {
+        pname = "kdl";
         version = "2.1.3";
-        # Replace after first build failure
-        hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
-        }
-      ];
-    };
-  };
+
+        src = pkgs.fetchurl {
+          url = "https://open-vsx.org/api/v1hz/kdl/2.1.3/file/v1hz.kdl-2.1.3.vsix";
+          hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+        };
+
+        vscodeExtPublisher = "v1hz";
+        vscodeExtName = "kdl";
+        vscodeExtUniqueId = "v1hz.kdl";
+      };
+    in {
+      programs.vscodium = {
+        enable = true;
+
+        profiles.default.extensions =
+          (with pkgs.vscode-extensions; [
+            enkia.tokyo-night
+            esbenp.prettier-vscode
+            jeff-hykin.better-nix-syntax
+          ]
+        )
+          ++ [ kdl ];
+      };
+
+    }
+
+  );
 }
-
-
-
