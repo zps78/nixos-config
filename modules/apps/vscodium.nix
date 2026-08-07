9169ae1ce459db334a1d9c2a6ac324bb712637b6
zp <mostly@kuro>
2026-08-07 12:34:36 +0100

    vscodium extension

--- modules/apps/vscodium.nix
+++ modules/apps/vscodium.nix
@@ -8,17 +8,19 @@
   config = lib.mkIf config.myApps.vscodium.enable {
     programs.vscodium =
       let
-        kdl = pkgs.vscode-utils.buildVscodeExtension {
-          pname = "kdl";
-          version = "2.1.3";
-          src = pkgs.fetchurl {
-            url = "https://open-vsx.org/api/v1hz/kdl/2.1.3/file/v1hz.kdl-2.1.3.vsix";
-            hash = "sha256-i5J4hXU3cOnFiPS+LZ0fIwwQo7hV0dueF0cPXUv25z0=";
-          };
-          vscodeExtPublisher = "v1hz";
-          vscodeExtName = "kdl";
-          vscodeExtUniqueId = "v1hz.kdl";
-        };
+        customExtensions = [
+          (pkgs.vscode-utils.buildVscodeExtension {
+            pname = "kdl";
+            version = "2.1.3";
+            src = pkgs.fetchurl {
+              url = "https://open-vsx.org/api/v1hz/kdl/2.1.3/file/v1hz.kdl-2.1.3.vsix";
+              hash = "sha256-i5J4hXU3cOnFiPS+LZ0fIwwQo7hV0dueF0cPXUv25z0=";
+            };
+            vscodeExtPublisher = "v1hz";
+            vscodeExtName = "kdl";
+            vscodeExtUniqueId = "v1hz.kdl";
+          })
+        ];
       in {
         enable = true;
         profiles.default.extensions =
@@ -27,7 +29,7 @@
             esbenp.prettier-vscode
             jeff-hykin.better-nix-syntax
           ])
-        ++ [ kdl ];
+        ++ customExtensions;
       };
   };
 }
