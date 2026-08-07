efdc53b4c33d3cef8071c2aaef8f98403e5f58e2
zp <mostly@kuro>
2026-08-07 10:45:03 +0100

    vscodium extension

--- modules/apps/vscodium.nix
+++ modules/apps/vscodium.nix
@@ -9,11 +9,24 @@
     programs.vscodium = {
       enable = true;
 
-      profiles.default.extensions = with pkgs.vscode-extensions; [
+    profiles.default.extensions =
+      (with pkgs.vscode-extensions; [
         enkia.tokyo-night
         esbenp.prettier-vscode
         jeff-hykin.better-nix-syntax
+        ]
+      )
+      ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+        {
+          name = "kdl";
+          publisher = "kdl-org";
+          version = "2.1.0";
+          hash = lib.fakeHash;
+        }
       ];
     };
   };
 }
+
+
+
