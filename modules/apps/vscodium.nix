3d66f09843bfa8d3eb51d557a88f6a16bddeb803
zp <mostly@kuro>
2026-08-07 12:23:30 +0100

    vscodium extension

--- modules/apps/vscodium.nix
+++ modules/apps/vscodium.nix
@@ -14,7 +14,7 @@
 
           src = pkgs.fetchurl {
             url = "https://open-vsx.org/api/v1hz/kdl/2.1.3/file/v1hz.kdl-2.1.3.vsix";
-            hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+            hash = "sha256-i5J4hXU3cOnFiPS+LZ0fIwwQo7hV0dueF0cPXUv25z0=";
           };
 
           vscodeExtPublisher = "v1hz";
