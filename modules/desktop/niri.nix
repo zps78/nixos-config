c2fdc760c8f1e2e14696e7deff0c7af833cd198f
zp <mostly@kuro>
2026-08-08 08:39:33 +0100

    kde apps integration with niri

--- modules/desktop/niri.nix
+++ modules/desktop/niri.nix
@@ -65,11 +65,12 @@ lib.mkIf (config.myDesktop.stack == "niri") {
 #   xwayland-satellite       # Xwayland outside your Wayland compositor
   ];
 
-environment.etc."xdg/menus/applications.menu".source =
-  "${pkgs.kdePackages.plasma-workspace}/etc/xdg/menus/plasma-applications.menu";
+  environment.etc."xdg/menus/applications.menu".source =
+    "${pkgs.kdePackages.plasma-workspace}/etc/xdg/menus/plasma-applications.menu";
 
-environment.pathsToLink = [
-  "share/thumbnailers"
+  environment.pathsToLink = [
+    "share/thumbnailers"
+  ];
 
   environment.sessionVariables = {
     XDG_SESSION_DESKTOP = "niri";
