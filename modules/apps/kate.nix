7f9acb7d5cd49556449a98a81ae90e45b043d9c7
zp <mostly@kuro>
2026-08-09 16:23:21 +0100

    mime setup

--- modules/apps/kate.nix
+++ modules/apps/kate.nix
@@ -15,10 +15,34 @@
       marksman
       nil
     ];
+
+  # ----------------------
+  # KDL syntax highlighting
+  # ----------------------
     xdg.dataFile."org.kde.syntax-highlighting/syntax/kdl.xml".source =
       pkgs.fetchurl {
         url = "https://raw.githubusercontent.com/larsgw/katepart-kdl/main/kdl.xml";
         hash = "sha256-Hb0lChLsRstZQ02I1A/J7KWQ6OISMw7ji3zUNGWONu4=";
       };
+
+  # ----------------------
+  # KDL MIME type
+  # ----------------------
+    xdg.dataFile."mime/packages/kdl.xml".text = ''
+      <?xml version="1.0" encoding="UTF-8"?>
+      <mime-info xmlns="http://www.freedesktop.org/standards/shared-mime-info">
+        <mime-type type="application/vnd.kdl">
+          <comment>KDL document</comment>
+          <glob pattern="*.kdl"/>
+        </mime-type>
+      </mime-info>
+    '';
+
+  # ----------------------
+  # Default application
+  # ----------------------
+    xdg.mimeApps.defaultApplications = {
+      "application/vnd.kdl" = "org.kde.kate.desktop";
+    };
   };
 }
