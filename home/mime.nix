c6e707d6248e8355057d19b49f067312ca493d64
zp <mostly@kuro>
2026-08-14 16:48:31 +0100

    zen browser as default

--- home/mime.nix
+++ home/mime.nix
@@ -66,6 +66,16 @@
       "application/vnd.kdl"            = "org.kde.kate.desktop";
 
       # ==========================
+      # Web
+      # ==========================
+
+      "text/html"                      = "zen.desktop";
+      "x-scheme-handler/http"          = "zen.desktop";
+      "x-scheme-handler/https"         = "zen.desktop";
+      "x-scheme-handler/about"         = "zen.desktop";
+      "x-scheme-handler/unknown"       = "zen.desktop";
+
+      # ==========================
       # Microsoft Office
       # ==========================
 
