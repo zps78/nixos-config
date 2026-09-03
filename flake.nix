9332f38c101040bcc4f3b13e872599dc28be00f7
zp <mostly@kuro>
2026-09-03 18:16:34 +0100

    spotatui + plex-tui

--- flake.nix
+++ flake.nix
@@ -25,6 +25,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    plex-tui = {
+      url = "github:so1omon563/plex-tui";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
