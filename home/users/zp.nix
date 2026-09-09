14c6de7325e0df7c10ece9ccf7cafadf996798df
zp <mostly@kuro>
2026-09-09 17:25:57 +0100

    update zp app choice

--- home/users/zp.nix
+++ home/users/zp.nix
@@ -29,9 +29,9 @@
   myApps = {
 
     # 3d modeling
-    bambu-studio.enable                          = true;             # PC Software for BambuLab's 3D printers
+    bambu-studio.enable                          = false;            # PC Software for BambuLab's 3D printers
     blender.enable                               = false;            # 3D Creation/Animation/Publishing System
-    f3d.enable                                   = false;            # Fast minimalist 3D viewer (VTK)
+    f3d.enable                                   = true;             # Fast minimalist 3D viewer (VTK)
     freecad.enable                               = true;             # General purpose Open Source 3D CAD/MCAD/CAx/CAE/PLM modeler
     openscad.enable                              = false;            # 3D parametric model compiler
     orca-slicer.enable                           = true;             # G-code generator for 3D printers
